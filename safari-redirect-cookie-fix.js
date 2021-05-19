2cb57a6bf94c77aa7c982a2bdcba55218bb860b2
codementor-safari-redirect-workaround <84428004+codementor-safari-redirect-workaround@users.noreply.github.com>
2021-05-19 07:48:42 +0530

    Create safari-redirect-cookie-fix.js

---
+++ safari-redirect-cookie-fix.js
@@ -0,0 +1,23 @@
+const thirdPartySafariRedirectWorkaround = {
+  setCookie: function setCookie(name, value) {
+    const d = new Date();
+		const year = d.getFullYear();
+		const month = d.getMonth();
+		const day = d.getDate();
+    const expDate = new Date(year + 10, month, day);
+		const cookieValue = typeof value !== "undefined" ? value : "";
+		cookieValue += ";expires=" + expDate.toGMTString() + ";path=/";
+		document.cookie = name + "=" + cookieValue;
+  },
+  
+  isSafari: function isSafari() {
+    // https://stackoverflow.com/a/7768006/421891
+    return /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+  }
+};
+
+if (
+  thirdPartySafariRedirectWorkaround.isSafari()
+) {
+  thirdPartySafariRedirectWorkaround.setCookie('safari_fix_applied', new Date().toISOString())  
+}
